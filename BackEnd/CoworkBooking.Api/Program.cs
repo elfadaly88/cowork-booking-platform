dcf00a30416a9bb7dfc5e6eab2285957b869cc69
elfadaly88 <mostafa.sayed@miuegypt.edu.eg>
2025-10-20 01:21:42 +0300

    new api modifications

--- BackEnd/CoworkBooking.Api/Program.cs
+++ BackEnd/CoworkBooking.Api/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CoworkBooking API",
+        Version = "v1",
+        Description = "API documentation for Cowork Booking Platform"
+    });
+});
 
 var app = builder.Build();
 
